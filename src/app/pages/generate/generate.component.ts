d6164eebb0ce88db81987fb023492f97672d31ec
Javier Garcia <jav.garcia.ext@orangebank.es>
2023-07-31 00:23:58 +0200

    Refactor

--- src/app/pages/generate/generate.component.ts
+++ src/app/pages/generate/generate.component.ts
@@ -1,6 +1,46 @@
 import { Component } from '@angular/core';
 import { UtilsService } from '../../utils/utils.service';
+import { Pair } from '../../modules/card/interfaces/card';
 
+// TODO: fill from screen input and do a request to MongoDB server to update the Data Base
+// Add new elements to generate the cards
+const pairs: Pair[] = [
+  {
+    icon: 'home',
+    es: 'Casa',
+    en: 'Home'
+  },
+  {
+    icon: 'directions_car',
+    es: 'Coche',
+    en: 'Car'
+  },
+  {
+    icon: 'wb_cloudy',
+    es: 'Nube',
+    en: 'Cloud'
+  },
+  {
+    icon: 'beach_access',
+    es: 'Playa',
+    en: 'Beach'
+  },
+  {
+    icon: 'flight',
+    es: 'Avión',
+    en: 'Plane'
+  },
+  {
+    icon: 'arrow_forward',
+    es: 'Flecha',
+    en: 'Arrow'
+  },
+  {
+    icon: 'close',
+    es: 'Cruz',
+    en: 'Cross'
+  }
+];
 
 @Component({
   selector: 'app-generate',
@@ -14,6 +54,6 @@ export class GenerateComponent {
 
   // Generate array of pairs
   generateJSON() {
-    this.generatedJSON = JSON.stringify(this.utilsService.generateCards());
+    this.generatedJSON = JSON.stringify(this.utilsService.generateCards(pairs));
   }
 }
--- src/app/utils/utils.service.ts
+++ src/app/utils/utils.service.ts
@@ -1,48 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Card, Pair } from '../modules/card/interfaces/card';
 
-// Add new elements to generate the cards
-export const pairs: Pair[] = [
-  {
-    icon: 'home',
-    es: 'Casa',
-    en: 'Home'
-  },
-  {
-    icon: 'directions_car',
-    es: 'Coche',
-    en: 'Car'
-  },
-  {
-    icon: 'wb_cloudy',
-    es: 'Nube',
-    en: 'Cloud'
-  },
-  {
-    icon: 'beach_access',
-    es: 'Playa',
-    en: 'Beach'
-  },
-  {
-    icon: 'flight',
-    es: 'Avión',
-    en: 'Plane'
-  },
-  {
-    icon: 'arrow_forward',
-    es: 'Flecha',
-    en: 'Arrow'
-  },
-  {
-    icon: 'close',
-    es: 'Cruz',
-    en: 'Cross'
-  }
-];
-
 @Injectable()
 export class UtilsService {
-  generateCards(): Card[] {
+  generateCards(pairs: Pair[]): Card[] {
     return pairs.flatMap((pair: Pair, index) => {
       return [
         {
