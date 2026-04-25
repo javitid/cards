645326c390807e840f4264f33bb7273a712a0c26
Javier Garcia <javitid@gmail.com>
2026-04-26 00:48:14 +0200

    fix: use Node.js script for Firebase injection to avoid shell syntax issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   appId: 'cards-397007',
   firebase: {
-    apiKey: 'PLACEHOLDER_FIREBASE_API_KEY',
-    authDomain: 'PLACEHOLDER_FIREBASE_AUTH_DOMAIN',
-    projectId: 'PLACEHOLDER_FIREBASE_PROJECT_ID',
-    storageBucket: 'PLACEHOLDER_FIREBASE_STORAGE_BUCKET',
-    messagingSenderId: 'PLACEHOLDER_FIREBASE_MESSAGING_SENDER_ID',
-    appId: 'PLACEHOLDER_FIREBASE_APP_ID',
-    measurementId: 'PLACEHOLDER_FIREBASE_MEASUREMENT_ID'
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_AUTH_DOMAIN',
+    projectId: 'FIREBASE_PROJECT_ID',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+    appId: 'FIREBASE_APP_ID',
+    measurementId: 'FIREBASE_MEASUREMENT_ID'
   },
   openAICredentials: {
     apiKey: '',
