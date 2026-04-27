94d9f3100fe09364358e02341bb67d5bbd5f6c47
Javier Garcia <javitid@gmail.com>
2026-04-27 13:26:20 +0200

    Refine compact mobile header for game board

--- src/app/modules/card/components/card-container/card-container.component.ts
+++ src/app/modules/card/components/card-container/card-container.component.ts
@@ -45,6 +45,7 @@ export class CardContainerComponent implements OnDestroy {
   isFlipEffect = DEFAULT_FLIP_EFFECT;
   isHeaderFixed = false;
   isLastCardSelected = false;
+  isMobilePanelOpen = false;
   isSelectionBlocked = false;
   isTwoColumns: boolean;
   isLoading = true;
@@ -67,6 +68,7 @@ export class CardContainerComponent implements OnDestroy {
     private readonly cdr: ChangeDetectorRef
   ) {
     this.isTwoColumns = helperService.isSmallScreen || DEFAULT_TWO_COLUMNS;
+    this.isMobilePanelOpen = !helperService.isSmallScreen;
     this.loadCards();
   }
 
@@ -105,6 +107,10 @@ export class CardContainerComponent implements OnDestroy {
     this.isTwoColumns = !this.isTwoColumns;
     this.rebuildBoard();
     this.closeMenu();
+  }
+
+  toggleMobilePanel(): void {
+    this.isMobilePanelOpen = !this.isMobilePanelOpen;
   }
 
   ngOnDestroy(): void {
